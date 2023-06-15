fc3d33b70fcf6973f53576aabc7c7e2bd710df84
sayedfmurad <49248799+sayedfmurad@users.noreply.github.com>
2023-06-15 15:26:56 +0200

    awef

--- pages/zusatzstoffe.js
+++ pages/zusatzstoffe.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import MyNavbar from "../components/NavBar/MyNavbar"
+import MyNavbar from "../components/TopBar/TobBar2"
 import langswitch from "../components/Utils/langswitch"
 export default function Name() {
 
