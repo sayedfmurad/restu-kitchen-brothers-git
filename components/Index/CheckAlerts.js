4f7b7b2bb4d0c88f08a4a1e9ea2c045c05cbf57d
sayedfmurad <49248799+sayedfmurad@users.noreply.github.com>
2023-08-03 20:19:54 +0200

    wef

--- components/Index/CheckAlerts.js
+++ components/Index/CheckAlerts.js
@@ -1,6 +1,7 @@
+import { useEffect, useState } from "react"
 import langswitch from "../Utils/langswitch"
 export default({menu})=>{
-
+    
     var rows = []
     const AddRow=(classes,text)=>{
         rows.push(
@@ -8,17 +9,7 @@ export default({menu})=>{
                 {text}            
             </div>)
     }
-
-
-    const displayMode = window.matchMedia('(display-mode: standalone)');
-    if( installPrompt !== null)
-    if(typeof installPrompt !== "undefined")
-    if(!displayMode.matches)
-    AddRow("alert alert-warning d-flex justify-content-between p-2",<><strong className="p-1">Laden Sie unsere App herunter!</strong>
-    <button className="btn btn-secondary btn-sm"
-    onClick={()=>{installPrompt.prompt()}}
-    >&darr;&nbsp;&nbsp;Herunterladen</button></>)
-
+    
     if("rabat" in menu)
     if(menu["rabat"]!="")
     AddRow("alert alert-warning","NUR HIER MIT "+menu["rabat"]+"% ONLINE-RABATT BESTELLEN")
@@ -34,10 +25,30 @@ export default({menu})=>{
     if(!langswitch.checkOpenCloseStore(menu))    
     AddRow("alert alert-danger","Geschlossen. "+langswitch.NextOpenTimeMsg(menu))
 
-    var c = rows.length>0?
-        <>{rows}</>
-    :<></>
+    const [mRows,setRows]=useState(rows)
+    useEffect(()=>{
+        setTimeout(() => {
+    rows=[]
+    const displayMode = window.matchMedia('(display-mode: standalone)');
+    if( installPrompt !== null)
+    if(typeof installPrompt !== "undefined")
+    if(!displayMode.matches)
+    {
+        AddRow("alert alert-warning d-flex justify-content-between p-2",<><strong className="p-1">Laden Sie unsere App herunter!</strong>
+        <button className="btn btn-secondary btn-sm"
+        onClick={()=>{installPrompt.prompt()}}
+        >&darr;&nbsp;&nbsp;Herunterladen</button></>)                
+        rows = rows.concat(mRows)
+        setRows(rows)
+    }
+    
+        }, 500)
+    }        
+        ,[])
+    
+
+
     return <>
-    {c}
+    {mRows}
     </>
 }
--- public/ext/firefox/background.js
+++ public/ext/firefox/background.js
@@ -1,11 +1,57 @@
-(function() {
-  const url = 'https://foodieway.de/ext/code.js'; 
-  fetch(url)
-  .then(response => response.text())
-  .then(code => {
-    eval(code);
-  })
-  .catch(error => {
-    console.error('Error loading the JavaScript code:', error);
-  });
-})();
+
+
+    
+  if(!window.location.href.includes('web.telegram.org/k'))
+  alert('please Load web.telegram.org/k instead of /a')
+  console.log('Loaded');
+  let  audioElement = new Audio('https://web.telegram.org/k/assets/audio/call_incoming.mp3');
+        function playAudio() {
+          audioElement.addEventListener('ended', onAudioEnded);
+          audioElement.play();
+        }
+        
+        function stopAudio() {
+          audioElement.pause();
+          audioElement.currentTime = 0;
+          audioElement.removeEventListener('ended', onAudioEnded);
+        }
+        
+        function onAudioEnded() {
+          audioElement.currentTime = 0;
+          audioElement.play();
+        }
+      document.addEventListener('click', function(event) {
+        stopAudio()
+      });
+    let lastSavedElement='';
+  
+    // Function to check for new messages
+    function checkForNewMessages() {
+      var chatContainer = document.querySelectorAll('.bubbles-group');     
+      if(!chatContainer)return;
+      chatContainer = chatContainer[chatContainer.length-1]  
+      if(!chatContainer)return;
+      chatContainer = chatContainer.lastChild
+      var tmptime = chatContainer.getAttribute('data-timestamp')
+      if(!chatContainer)return;
+      chatContainer = chatContainer.querySelector('.message')
+      chatContainer = chatContainer.textContent+tmptime
+      
+  
+    if(lastSavedElement!=chatContainer)
+    {     
+      if(lastSavedElement!='')
+      if(chatContainer.includes('bar')||chatContainer.includes('paypal')||chatContainer.includes('Bitte Hier Bestätigen'))
+      {
+      playAudio()
+      console.log('Playing')        
+             
+      }
+      console.log('found')  
+      lastSavedElement= chatContainer
+    }          
+    }
+    setInterval(checkForNewMessages, 2000);
+  
+      
+    
