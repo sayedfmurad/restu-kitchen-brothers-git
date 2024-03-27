e12b33e0270ae101d446594529f3af9b061fe464
sayedfmurad <49248799+sayedfmurad@users.noreply.github.com>
2024-03-27 21:25:12 +0100

    wef

--- components/Index/customizeorder.js
+++ components/Index/customizeorder.js
@@ -471,7 +471,7 @@ export function IDisReady ({menu, id, orderid , orders}) {
         <svg viewBox="0 0 16 16" width="1em" height="1.66em" role="presentation" focusable="false" aria-hidden="true"><path fill="white" d="M14.125 7.344H8.656V1.875H7.344v5.469H1.875v1.312h5.469v5.469h1.312V8.656h5.469V7.344z"></path></svg>
             </a>
         </div>
-<button style={{minHeight:"56px","minWidth":'150px',fontSize:"24px"}} onClick={addOrder} class="border-success rounded-5 btn btn-success btn-lg ">
+<button style={{maxWidth:"45%",width:"100%",minHeight:"56px","minWidth":'150px',fontSize:"24px"}} onClick={addOrder} class="border-success rounded-5 btn btn-success btn-lg ">
    {addButtonText}</button>
 </div>
 
